b3a097f1bdb3a5db6e3f5502a9ce2ee0f0e4a409
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-02 11:41:02 +0000

    Adds static holding page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  siteMetadata: {
-    title: `Destiny Clan Wars`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-stylus`,
+      resolve: 'gatsby-plugin-stylus',
       options: {
-        use: [],
-      },
+        use: []
+      }
     },
-  ],
+    '@jacobmischka/gatsby-plugin-react-svg'
+  ]
 }

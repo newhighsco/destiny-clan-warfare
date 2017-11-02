2501ea28ba3ccfd9bab375ae296a4c231b45ebd7
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-02 15:48:05 +0000

    > Adds POC for getting clan information from APIs

---
+++ src/html.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+class Html extends Component {
+  render () {
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="initial-scale=1"
+          />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
+
+Html.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.node.isRequired,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.node.isRequired,
+  body: PropTypes.node.isRequired,
+  postBodyComponents: PropTypes.node.isRequired
+}
+
+export default Html
---
+++ src/templates/clan.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ClanTemplate extends Component {
+  render () {
+    const currentPage = this.props.data.clan
+
+    return (
+      <div>
+        <h1>Clan</h1>
+        <p dangerouslySetInnerHTML={{ __html: currentPage.name }} />
+      </div>
+    )
+  }
+}
+
+ClanTemplate.propTypes = {
+  data: PropTypes.object
+}
+
+export default ClanTemplate
+
+export const pageQuery = graphql`
+  query currentPageQuery($id: String!) {
+    clan(id: { eq: $id }) {
+      name
+    }
+  }
+`

7a75094f72227ee64c0710daea6556f1290dffe6
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-07 10:18:12 +0000

    Improves 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import HoldingPage from '../components/HoldingPage/HoldingPage'
 import { Logo } from '../components/Logo/Logo'
 
-const NotFoundPage = () => (
-  <HoldingPage>
-    <Helmet
-      meta={[
-        { name: 'robots', content: 'noindex,nofollow' }
-      ]}
-    />
-    <Logo />
-  </HoldingPage>
-)
+class NotFoundPage extends Component {
+  render () {
+    return (
+      <HoldingPage>
+        <Helmet>
+          <title>Page not found</title>
+          <meta name="robots" content="noindex,nofollow" />
+        </Helmet>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </HoldingPage>
+    )
+  }
+}
 
 export default NotFoundPage

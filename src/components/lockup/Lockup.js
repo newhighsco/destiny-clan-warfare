37d90aa1a5dd80fcf366c88f99f18fc2c3b2ecd6
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 22:06:39 +0000

    > Adds link to lockups

--- src/components/lockup/Lockup.js
+++ src/components/lockup/Lockup.js
@@ -1,23 +1,42 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-const Lockup = ({ children, heading, kicker, className, element }) => {
-  const baseClassName = 'lockup'
-  const Element = element
+const baseClassName = 'lockup'
+
+const Lockup = ({ children, href, heading, kicker, className, element }) => {
+  const absoluteUrlRegEx = /^\w[\w-.+]+:/ // http://regexr.com/3fhfg
+  const commonAttributes = {
+    className: classNames(baseClassName, className)
+  }
+
+  if (!href) {
+    return (
+      <span {...commonAttributes}>
+        <LockupElement elementName="kicker">{kicker}</LockupElement>
+        <LockupElement element={element} elementName="heading">{heading}</LockupElement>
+        <LockupElement elementName="content">{children}</LockupElement>
+      </span>
+    )
+  }
+
+  if (absoluteUrlRegEx.exec(href)) {
+    return (
+      <a href={href} {...commonAttributes}>
+        <LockupElement elementName="kicker">{kicker}</LockupElement>
+        <LockupElement element={element} elementName="heading">{heading}</LockupElement>
+        <LockupElement elementName="content">{children}</LockupElement>
+      </a>
+    )
+  }
 
   return (
-    <span className={classNames(baseClassName, className)}>
-      {kicker &&
-        <span className="lockup__kicker">{kicker} </span>
-      }
-      <Element className="lockup__heading">{heading}</Element>
-      {children &&
-        <span className="lockup__content">
-          {children}
-        </span>
-      }
-    </span>
+    <Link to={href} {...commonAttributes}>
+      <LockupElement elementName="kicker">{kicker}</LockupElement>
+      <LockupElement element={element} elementName="heading">{heading}</LockupElement>
+      <LockupElement elementName="content">{children}</LockupElement>
+    </Link>
   )
 }
 
@@ -27,10 +46,33 @@ Lockup.defaultProps = {
 
 Lockup.propTypes = {
   children: PropTypes.node,
-  heading: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  heading: PropTypes.string,
   kicker: PropTypes.string,
   className: PropTypes.string,
   element: PropTypes.string
+}
+
+const LockupElement = ({ children, element, elementName }) => {
+  const Element = element
+
+  if (children) {
+    return (
+      <Element className={`${baseClassName}__${elementName}`}>{children}</Element>
+    )
+  }
+
+  return (null)
+}
+
+LockupElement.defaultProps = {
+  element: 'span'
+}
+
+LockupElement.propTypes = {
+  children: PropTypes.node,
+  element: PropTypes.string,
+  elementName: PropTypes.string
 }
 
 export default Lockup

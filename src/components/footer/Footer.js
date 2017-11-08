d28aadc56c24471e71bbc6855d1286c354024e6a
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 17:28:11 +0000

    > Fixes file name casing

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Credit from '../credit/Credit'
+
+const Footer = () => (
+  <footer className="footer" role="contentinfo">
+    <div className="content-center content-gutter">
+      <Credit />
+    </div>
+  </footer>
+)
+
+export default Footer

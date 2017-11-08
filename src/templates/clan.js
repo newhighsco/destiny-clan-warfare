5197308cb032d4a597147d2d06bc49885d3233c7
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 22:27:00 +0000

    > Pads out WIP pages

--- src/templates/clan.js
+++ src/templates/clan.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import PageContainer from '../components/page-container/PageContainer'
 import Card from '../components/card/Card'
+import Button from '../components/button/Button'
 // import Avatar from '../components/avatar/Avatar'
 import Lockup from '../components/lockup/Lockup'
 
@@ -22,11 +23,14 @@ class ClanTemplate extends Component {
           */}
           <Lockup kicker={data.clan.motto} heading={data.clan.name}>
             {data.clan.description &&
-              <p dangerouslySetInnerHTML={{ __html: data.clan.description }} />
+              <p dangerouslySetInnerHTML={{ __html: data.clan.description.replace(/\n/g, '<br />') }} />
             }
           </Lockup>
+          <div className="button-group">
+            <Button href={`https://www.bungie.net/en/ClanV2?groupid=${data.clan.id}`}>Bungie.net</Button>
+          </div>
         </Card>
-        <Lockup kicker="Members" />
+        <Lockup kicker="Leaderboard of sortable member stats to go here" />
         <ul>
           {data.allMember.edges.map(({ node }) => (
             <li key={node.id}>

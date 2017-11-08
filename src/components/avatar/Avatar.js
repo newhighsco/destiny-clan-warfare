d602ce2d0437f32615592598c78a5b6a43ba01c2
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 17:26:37 +0000

    Adds WIP avatar

---
+++ src/components/avatar/Avatar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import ResponsiveMedia from '../responsive-media/ResponsiveMedia'
+
+const Avatar = (props) => {
+  const { src, className } = props
+
+  return (
+    <div className={classNames('avatar', className)} {...props}>
+      <ResponsiveMedia ratio="1:1">
+        <img src={src} alt="" />
+      </ResponsiveMedia>
+    </div>
+  )
+}
+
+Avatar.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string
+}
+
+export default Avatar

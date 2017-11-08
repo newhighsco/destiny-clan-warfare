ca5ab8b5d167f3393ea11775aeb785341880ff40
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 17:26:57 +0000

    Adds Icon component

--- src/components/Credit/Credit.js
+++ src/components/Credit/Credit.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Icon from '../icon/Icon'
 import AvalancheUkSvg from '../../images/avalanche-uk.svg'
 
 const Credit = () => (
   <div className="credit">
     <a className="credit__link" href="https://avaclanche.uk" target="_blank" rel="noopener">
-      <AvalancheUkSvg className="credit__logo" />
+      <Icon className="credit__icon">
+        <AvalancheUkSvg />
+      </Icon>
       <div className="credit__details">
         <small>Proudly brought to you by</small>
         <br />

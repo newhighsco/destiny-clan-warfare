ba54b8f3b73fe022b9fba1393b4336b1cc23344d
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-08 17:26:57 +0000

    Adds Icon component

---
+++ src/components/icon/Icon.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+export default function Icon ({ children, a11yText, className, height, width }) {
+  const iconClassNames = classNames(
+    'icon',
+    { 'icon--custom-size': width || height },
+    className
+  )
+
+  return (
+    <i
+      className={iconClassNames}
+      aria-hidden={!a11yText ? 'true' : null}
+      style={{
+        width: width && `${width}px`,
+        height: height && `${height}px`,
+        lineHeight: height && `${height}px`
+      }}
+      >
+      {a11yText &&
+        <span className="is-vhidden">{a11yText}</span>
+      }
+      {children}
+    </i>
+  )
+}
+
+Icon.propTypes = {
+  children: PropTypes.node.isRequired,
+  a11yText: PropTypes.string,
+  className: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number
+}

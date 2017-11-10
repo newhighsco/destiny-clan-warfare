7796fb7e0c4783cc4eb516c2eab134cfb31d7fbb
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-10 01:03:39 +0000

    > Fixes linting errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import PageContainer from '../components/page-container/PageContainer'
 import Lockup from '../components/lockup/Lockup'
 
 class IndexPage extends Component {
   render () {
-    const { data } = this.props
-
     return (
       <PageContainer>
         <Lockup kicker="Beta site" heading="Pages" />
@@ -24,25 +21,8 @@ class IndexPage extends Component {
   }
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.object
-}
-
 export default IndexPage
 
 export const data = {
   layout: 'content'
 }
-
-export const pageQuery = graphql`
-  query IndexPageQuery {
-    allSitePage(sort: { fields: [ path ] }) {
-      edges {
-        node {
-          id
-          path
-        }
-      }
-    }
-  }
-`

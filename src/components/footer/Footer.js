a36ede62c05e016e4fb404d60219581d19e4e2cc
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-10 18:45:27 +0000

    Adds links and copyright to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import Credit from '../credit/Credit'
 
-const Footer = () => (
-  <footer className="footer" role="contentinfo">
-    <div className="content-center content-gutter">
-      <Credit />
-    </div>
-  </footer>
-)
+const Footer = () => {
+  const date = new Date()
+
+  return (
+    <footer className="footer" role="contentinfo">
+      <div className="content-center content-gutter">
+        <div className="grid grid--reverse grid--middled">
+          <div className="grid__item tablet-one-half">
+            <ul className="footer__list list--inline">
+              <li className="footer__item">
+                <Link className="footer__link" to="/clans">Clans</Link>
+              </li>
+              <li className="footer__item">
+                <Link className="footer__link" to="/members">Members</Link>
+              </li>
+              <li className="footer__item">
+                <span className="footer__link">&copy;{date.getFullYear()}</span>
+              </li>
+            </ul>
+          </div>
+          <div className="grid__item tablet-one-half">
+            <Credit />
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer

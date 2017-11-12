7a7783a168f3a9ff49e5e378d71439eb27442603
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-12 16:37:07 +0000

    Updates header and footer content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import { LogoIcon, LogoLockup } from '../logo/Logo'
+import { LogoLockup } from '../logo/Logo'
+import Icon from '../icon/Icon'
+import TwitterSvg from '../../images/twitter.svg'
+import DiscordSvg from '../../images/discord.svg'
+import PatreonSvg from '../../images/patreon.svg'
 
 const Header = () => (
   <header className="header" role="banner">
     <div className="header__container content-center content-gutter">
-      <LogoIcon size="medium" className="header__logo-icon" />
-      <Link className="header__logo-link" to="/">
-        <LogoLockup size="small" className="header__logo-lockup" />
-      </Link>
+      <div className="grid">
+        <div className="grid__item one-half">
+          <Link className="header__logo-link" to="/">
+            <LogoLockup size="small" className="header__logo-lockup" />
+          </Link>
+        </div>
+        <div className="grid__item one-half">
+          <ul className="header__list list--inline">
+            <li className="header__item">
+              <a className="header__link" href="https://twitter.com/destinyclanwar" target="_blank" rel="noopener">
+                <Icon className="header__icon" a11yText="Follow us on Twitter">
+                  <TwitterSvg />
+                </Icon>
+              </a>
+            </li>
+            <li className="header__item">
+              <a className="header__link" href="http://discord.destinyclanwarfare.com" target="_blank" rel="noopener">
+                <Icon className="header__icon" a11yText="Join our Discord server">
+                  <DiscordSvg />
+                </Icon>
+              </a>
+            </li>
+            <li className="header__item">
+              <a className="header__link" href="https://www.patreon.com/destinyclanwarfare" target="_blank" rel="noopener">
+                <Icon className="header__icon" a11yText="Become a Patron">
+                  <PatreonSvg />
+                </Icon>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </header>
 )

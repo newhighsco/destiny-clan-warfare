e05176afd4d6035d9dfb3fb0358dad8a0ba00d40
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-13 18:48:39 +0000

    > Adds event pages to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import PageContainer from '../components/page-container/PageContainer'
 import Lockup from '../components/lockup/Lockup'
 
 class IndexPage extends Component {
   render () {
+    const { data } = this.props
+
     return (
       <PageContainer>
         <Lockup kicker="Beta site" heading="Pages" />
@@ -15,10 +18,19 @@ class IndexPage extends Component {
           <li>
             <Link to="/members">Members</Link>
           </li>
+          {data.allEvent.edges.map(({ node }) => (
+            <li key={node.id}>
+              <Link to={node.path}>{node.name}</Link>
+            </li>
+          ))}
         </ul>
       </PageContainer>
     )
   }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.object
 }
 
 export default IndexPage
@@ -26,3 +38,17 @@ export default IndexPage
 export const data = {
   layout: 'content'
 }
+
+export const pageQuery = graphql`
+  query IndexPageQuery {
+    allEvent(sort: { fields: [ startDate ], order: DESC }) {
+      edges {
+        node {
+          id
+          path
+          name
+        }
+      }
+    }
+  }
+`

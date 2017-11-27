e27e179b093f226de90a83f1a74eede22bea3bde
Ben Edwards <ben.edwards@clock.co.uk>
2017-11-27 18:36:16 +0000

    Optimises page queries further

--- src/templates/member.js
+++ src/templates/member.js
@@ -22,7 +22,7 @@ class MemberTemplate extends Component {
           {data.member.icon &&
             <Avatar className="card__avatar" icon={data.member.icon} />
           }
-          <Lockup center reverse kicker={data.clan.name} kickerHref={data.clan.path} heading={data.member.name} />
+          <Lockup center reverse kicker={data.member.clan.name} heading={data.member.name} />
           <Button key="button" href={`https://www.bungie.net/en/Profile/${data.member.id}`} target="_blank">View profile</Button>
           <MedalList key="medals" medals={[ { tier: 1, description: 'TBC' }, { tier: 2, description: 'TBC' }, { tier: 3, description: 'TBC' } ]} />
         </Card>
@@ -39,11 +39,14 @@ MemberTemplate.propTypes = {
 export default MemberTemplate
 
 export const pageQuery = graphql`
-  query MemberTemplateQuery($id: String!, $clanId: String!) {
+  query MemberTemplateQuery($id: String!) {
     member(id: { eq: $id }) {
       id
       name
       icon
+      clan {
+        name
+      }
       totals {
         wins
         kills
@@ -52,10 +55,6 @@ export const pageQuery = graphql`
         score
         lastPlayed
       }
-    }
-    clan(id: { eq: $clanId }) {
-      path
-      name
     }
   }
 `

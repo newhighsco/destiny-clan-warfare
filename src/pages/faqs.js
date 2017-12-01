771fe8d728745c73bb6b742140782fc4dbdb5a03
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-01 09:28:08 +0000

    > Adds title to FAQs page

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 import PageContainer from '../components/page-container/PageContainer'
 
 class FaqsPage extends Component {
   render () {
     return (
       <PageContainer>
+        <Helmet>
+          <title>FAQs</title>
+        </Helmet>
         <div className="temp">
           <p>FAQs</p>
         </div>

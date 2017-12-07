1dd9ecc54d480c6037251ff3bbbe34c0a36c2e7c
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-07 17:40:28 +0000

    > Fixes member pages

--- src/templates/member.js
+++ src/templates/member.js
@@ -38,8 +38,8 @@ class MemberTemplate extends Component {
             <Avatar className="card__avatar" icon={data.member.icon} />
           }
           <TagList tags={data.member.tags} className="card__tags" />
-          <Lockup primary center reverse kicker={data.member.clan.name} kickerHref={urlBuilder.clanUrl(data.member.clanId)} heading={data.member.name} />
-          <Button href={`https://www.bungie.net/en/Profile/${data.member.id}`} target="_blank">View profile</Button>
+          <Lockup primary center reverse kicker={data.member.clan.name} kickerHref={urlBuilder.clanUrl(data.member.clanId.substring(1))} heading={data.member.name} />
+          <Button href={`${constants.bungie.baseUrl}en/Profile/${data.member.id}`} target="_blank">View profile</Button>
           <MedalList medals={medals} />
           {lastPlayedDate > emptyDate &&
             <StatList stats={stats} />

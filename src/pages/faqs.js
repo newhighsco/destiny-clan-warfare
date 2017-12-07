c42a2ddfcdaa04f4d6770303a186cc74b1cc0ad2
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-07 17:41:09 +0000

    > Add basic structure to FAQs page

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import PageContainer from '../components/page-container/PageContainer'
+import Card from '../components/card/Card'
+import { Lockup } from '../components/lockup/Lockup'
 
 class FaqsPage extends Component {
   render () {
@@ -10,9 +12,12 @@ class FaqsPage extends Component {
           <title>FAQs</title>
           <meta name="description" content="Frequently asked questions about Destiny Clan Warfare" />
         </Helmet>
-        <div className="temp">
-          <p>FAQs</p>
-        </div>
+        <Card className="text-center">
+          <Lockup primary center kicker="Frequently asked" heading="Questions" />
+          <div className="temp">
+            <p>FAQs</p>
+          </div>
+        </Card>
       </PageContainer>
     )
   }

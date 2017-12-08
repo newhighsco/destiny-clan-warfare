9c91a68dd3ead8a772d4ca2cb32661f83679f223
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-08 08:11:19 +0000

    > Modifiers medal text to match what the backend will set

--- src/utils/medal-builder.js
+++ src/utils/medal-builder.js
@@ -2,12 +2,21 @@ var numberToWords = require('number-to-words')
 var sentenceCase = require('sentence-case')
 
 const build = (rank, tier, division) => {
-  const place = numberToWords.toWordsOrdinal(rank)
+  var place
+  var placeDescription
+
+  if (typeof rank === 'string') {
+    place = rank
+    placeDescription = `in the ${place} of`
+  } else {
+    place = numberToWords.toWordsOrdinal(rank)
+    placeDescription = `${place} place in`
+  }
 
   return {
     tier: tier,
     name: `${sentenceCase(place)} ${division}`,
-    description: `Finished ${place} place in the ${division} division`
+    description: `Finished ${placeDescription} a ${division} division event`
   }
 }
 
@@ -20,10 +29,8 @@ const embellishLeaderboard = (leaderboard, division) => {
         medal = build(1, 2, division)
         break
       case '#2':
-        medal = build(2, 1, division)
-        break
       case '#3':
-        medal = build(3, 1, division)
+        medal = build('top 3', 1, division)
         break
     }
 

40de7c81b6e9595fdd1db546e2dfdad837683b27
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-11 19:54:14 +0000

    Adds thank you page copy

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import PageContainer from '../components/page-container/PageContainer'
 import Card from '../components/card/Card'
 import { Lockup } from '../components/lockup/Lockup'
+import { Button } from '../components/button/Button'
+
+const queryString = require('query-string')
 
 class ThanksPage extends Component {
   render () {
+    const { location } = this.props
+    const query = queryString.parse(location.search)
+    const success = query.success ? JSON.parse(query.success.toLowerCase()) : true
+    const message = query.message || ''
+    const title = success ? 'Thanks for enrolling' : 'Enrollment failed'
+
     return (
       <PageContainer>
         <Helmet>
-          <title>Thanks</title>
+          <title>{title}</title>
           <meta name="description" content="Thanks for enrolling your clan in Destiny Clan Warfare" />
           <meta name="robots" content="noindex,nofollow" />
         </Helmet>
         <Card className="text-center">
-          <Lockup primary center kicker="Thanks for" heading="Enrolling" />
-          <div className="temp">
-            <p>Thanks copy</p>
-          </div>
+          {success ? ([
+            <Lockup key="lockup" primary center kicker="Thanks for" heading="Enrolling" />,
+            <p key="prose">Please allow <strong>60-90</strong> minutes for you clan and clan members to start appearing on the leaderboards</p>,
+            <Button key="button" href="/current">View current leaderboard</Button>
+          ]) : ([
+            <Lockup key="lockup" primary center kicker="Enrollment failed" heading="Please try again" />,
+            message &&
+              <p key="prose">{message}</p>,
+            <Button key="button" href="/#enroll">Enroll your clan today</Button>
+          ])}
         </Card>
       </PageContainer>
     )
   }
+}
+
+ThanksPage.propTypes = {
+  location: PropTypes.object
 }
 
 export default ThanksPage

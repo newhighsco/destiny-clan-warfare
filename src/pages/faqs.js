d3309237b6dbfa8d7091d3333c3a6fa0aa80289b
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-15 12:46:50 +0000

    Adds FAQs

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import PageContainer from '../components/page-container/PageContainer'
 import Card from '../components/card/Card'
 import { Lockup } from '../components/lockup/Lockup'
+import Prose from '../components/prose/Prose'
 
 class FaqsPage extends Component {
   render () {
@@ -14,7 +15,31 @@ class FaqsPage extends Component {
         </Helmet>
         <Card>
           <Lockup primary center kicker="Frequently asked" heading="Questions" />
-          <p>FAQs</p>
+          <Prose>
+            <h2>What is Destiny Clan Warfare?</h2>
+            <p>Destiny Clan Warfare is a website designed to be a clan vs clan weekly competition across a variety of game types with bonus point modifiers to keep it new and exciting each week.</p>
+            <h2>How do you earn points in Destiny Clan Warfare?</h2>
+            <p>Once your clan has been enrolled, you will automatically be tracked and added. You have nothing more to do.</p>
+            <h2>How many points do you earn in Destiny Clan Warfare?</h2>
+            <p>Unless stated otherwise, each event will be made up of the following points:</p>
+            <ul>
+              <li>100 points per kill</li>
+              <li>25 points per assist</li>
+              <li>-50 points per death</li>
+            </ul>
+            <p>You will also earn 2 sets of bonus points and a total score modifier which will vary each event. The active bonus & modifier will clearly be visible on the homepage.</p>
+            <h2>How do I enrol into Destiny Clan Warfare?</h2>
+            <p>At the top of the home page, you will be able to search for, and enroll, your clan. Please allow 60-90 minutes for you clan and clan members to start appearing on the leaderboards.</p>
+            <p>Please note - there is a limit on clan participation at this time so if we are full, please check back each week as we accept more clans.</p>
+            <h2>What if my clan is only small, I’ll never win anything?</h2>
+            <p>Whilst you may not win the tournament as a whole, everything we do is split into 3 divisions based on clan size increasing your chances of performing well.</p>
+            <p>The divisions are as follows:</p>
+            <ul>
+              <li>Squad: 1-30 members</li>
+              <li>Platoon: 31-60 members</li>
+              <li>Company: 61+ members</li>
+            </ul>
+          </Prose>
         </Card>
       </PageContainer>
     )

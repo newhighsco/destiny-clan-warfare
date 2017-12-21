9a7ed2ea658903a78d7fab55522698d24684a04a
Ben Edwards <ben.edwards@clock.co.uk>
2017-12-21 15:01:40 +0000

    Debugs environment variables

--- src/layouts/_master.js
+++ src/layouts/_master.js
@@ -30,6 +30,8 @@ class MasterLayout extends Component {
   }
 
   componentDidMount () {
+    console.log(process.env)
+
     this.setState({ enableIdentity: JSON.parse(process.env.ENABLE_IDENTITY) })
     identity.init()
   }

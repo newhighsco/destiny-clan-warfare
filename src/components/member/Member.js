4aecbf54b1f111a34bd470845b9113e4e9c4a55d
Ben Edwards <ben.edwards@clock.co.uk>
2018-02-08 19:46:11 +0000

    Further WIP on past member events

--- src/components/member/Member.js
+++ src/components/member/Member.js
@@ -6,10 +6,12 @@ import Card from '../card/Card'
 import Avatar from '../avatar/Avatar'
 import { Lockup } from '../lockup/Lockup'
 import { MedalList } from '../medal/Medal'
-import { StatList, StatHistory } from '../stat/Stat'
+import { StatList } from '../stat/Stat'
 import { Button } from '../button/Button'
 import { TagList } from '../tag/Tag'
 import Notification from '../notification/Notification'
+import Leaderboard from '../leaderboard/Leaderboard'
+import { TabContainer, Tab } from '../tab/Tab'
 
 const moment = require('moment')
 const constants = require('../../utils/constants')
@@ -25,8 +27,8 @@ class Member extends Component {
     const lastPlayedDate = moment.utc(totals.lastPlayed).format(constants.dateFormat)
     const stats = {
       events: 'TBC',
-      highestClanRank: 'TBC',
-      highestEventRank: 'TBC',
+      highestRank: 'TBC',
+      highestOverallRank: 'TBC',
       ...totals,
       lastPlayed: lastPlayedDate
     }
@@ -57,9 +59,9 @@ class Member extends Component {
       ]
     }
     const events = [
-      { id: 16, name: 'Some event' },
-      { id: 15, name: 'Another event with a longer title that wraps over new lines' },
-      { id: 17, name: 'Another event' }
+      { id: 16, game: { result: true, path: '/16', type: 'Iron Banner', map: 'Ended 4 days ago' }, medals: medals.slice(1, 2), rank: 7, overall: 140, games: 10, wins: 2, kda: 7.1, bonus1: 10, bonus2: 12, score: 2000000 },
+      { id: 15, game: { result: true, path: '/15', type: 'Competitive Midweek', map: 'Ended 10 days ago' }, medals: medals.slice(1, 3), rank: 1, overall: 1, games: 102, wins: 72, kda: 107.1, bonus1: 30, bonus2: 5, score: 2000 },
+      { id: 22, game: { result: true, path: '/22', type: '24 Hour Strike Race', map: 'Ended 40 days ago' }, medals: medals, rank: 72, overall: 4000, games: 120, wins: 24, kda: 700.1, bonus1: 100, bonus2: 1, score: 50500 }
     ]
     const hasEvents = events.length > 0
 
@@ -90,7 +92,11 @@ class Member extends Component {
           }
         </Card>
         {hasEvents &&
-          <StatHistory cutout events={events} />
+          <TabContainer cutout>
+            <Tab name="Events">
+              <Leaderboard data={events} className="leaderboard--history" />
+            </Tab>
+          </TabContainer>
         }
       </PageContainer>
     )

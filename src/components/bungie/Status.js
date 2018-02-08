9006cf20df5fddb90e6548ffa29e8df59422e265
Ben Edwards <ben.edwards@clock.co.uk>
2018-02-08 11:33:02 +0000

    Revert "Removes Bungie live status check to fix quota violation"
    This reverts commit cb42ae5cfdb88ca8a167e1bbc0eabe7f5868a243.

--- src/components/bungie/Status.js
+++ src/components/bungie/Status.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Notification from '../notification/Notification'
 
 const constants = require('../../utils/constants')
+const bungie = require('../../utils/bungie-helper')
+const httpExceptionHandler = require(`../../utils/http-exception-handler`)
 
-const Status = ({ code }) => {
-  const active = code === constants.bungie.disabledStatusCode
+class Status extends Component {
+  constructor (props) {
+    super(props)
 
-  if (!active) return null
+    const { code } = this.props
 
-  return (
-    <div className="content-center content-gutter">
-      <Notification state="error">
-        The Bungie API is currently offline for maintenance.
-      </Notification>
-    </div>
-  )
+    this.state = {
+      active: code === constants.bungie.disabledStatusCode
+    }
+  }
+
+  componentDidMount () {
+    bungie(`/Destiny2/Milestones/`)
+      .then(({ data }) => {
+        this.setState({ active: data.ErrorCode === constants.bungie.disabledStatusCode })
+      })
+      .catch(err => httpExceptionHandler(err))
+  }
+
+  render () {
+    const { active } = this.state
+
+    if (!active) return null
+
+    return (
+      <div className="content-center content-gutter">
+        <Notification state="error">
+          The Bungie API is currently offline for maintenance.
+        </Notification>
+      </div>
+    )
+  }
 }
 
 Status.propTypes = {

3cffb59e439714fc16956af0e65f4f6626081174
Ben Edwards <ben.edwards@clock.co.uk>
2018-02-16 13:35:55 +0000

    Adds KD to stats

--- src/utils/kda.js
+++ src/utils/kda.js
@@ -1,8 +1,9 @@
-const kda = (member, precision) => {
+const kda = (member, excludeAssists, precision) => {
   precision = precision || 2
 
   const deaths = member.deaths > 0 ? member.deaths : 1
-  const result = (member.kills + member.assists) / deaths
+  const assists = !excludeAssists ? member.assists : 0
+  const result = (member.kills + assists) / deaths
   const factor = Math.pow(10, precision)
 
   return Math.round(result * factor) / factor

4ed549792f0a8e1532f4ca523c462ccad38b84ae
Ben Edwards <ben.edwards@clock.co.uk>
2018-02-23 14:09:44 +0000

    Adds new Insider modifier icons

--- src/components/modifier/icons/index.js
+++ src/components/modifier/icons/index.js
@@ -10,14 +10,17 @@ import Merciless from './merciless.svg'
 import NiceThrow from './nice-throw.svg'
 import Perfection from './perfection.svg'
 import PewPewPew from './pew-pew-pew.svg'
+import RaidMaster from './raid-master.svg'
 import RockyBalboaSTrainee from './rocky-balboa-s-trainee.svg'
 import StrikeDeathPenalty from './strike-death-penalty.svg'
 import TelestoFesto from './telesto-festo.svg'
+import TwentyTwenty from './twenty-twenty.svg'
 import VarietyIsTheSpiceOfLife from './variety-is-the-spice-of-life.svg'
 import WinnerWinnerChickenDinner from './winner-winner-chicken-dinner.svg'
 import ZoneCaptured from './zone-captured.svg'
 
 const svgs = {
+  '20_20': { svg: TwentyTwenty, designer: 'Chanut is Industries' },
   AbuseOfPower: { svg: AbuseOfPower, designer: 'naim' },
   AntiMeta: { svg: AntiMeta, designer: 'TMD' },
   BandTogether: { svg: BandTogether, designer: 'Gabriel Vogel' },
@@ -30,6 +33,7 @@ const svgs = {
   NiceThrow: { svg: NiceThrow, designer: 'Hea Poh Lin' },
   Perfection: { svg: Perfection, designer: 'Montu Yadav' },
   PewPewPew: { svg: PewPewPew, designer: 'Nikol Kokešová' },
+  RaidMaster: { svg: RaidMaster, designer: 'Robert Bjurshagen' },
   RockyBalboaSTrainee: { svg: RockyBalboaSTrainee, designer: 'miza bin' },
   StrikeDeathPenalty: { svg: StrikeDeathPenalty, designer: 'Robert Bjurshagen' },
   TelestoFesto: { svg: TelestoFesto, designer: 'Casper Jensen' },

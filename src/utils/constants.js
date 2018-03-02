3b997efa70a6c5d6693f5d40568c7514ff2eb74c
Ben Edwards <hello@benedfit.com>
2018-03-02 15:34:41 +0000

    > Changes manifest short name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const bungieBaseUrl = 'https://www.bungie.net/'
 const constants = {
   meta: {
     name: 'Destiny Clan Warfare',
-    shortName: 'D2 Clan Wars',
+    shortName: 'Clan Warfare',
     title: 'Destiny Clan Warfare - Band together, Guardians',
     description: 'Wage war against other clans in Destiny 2 and battle your way to the top of the Destiny 2 clan leaderboard',
     handle: '@destinyclanwar',

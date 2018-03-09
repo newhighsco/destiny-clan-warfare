07b2d26471ea1eed2ffdaacd137676a99a07f5f4
Ben Edwards <ben.edwards@clock.co.uk>
2018-03-09 12:44:45 +0000

    Maintain consistent spacing when enrollment is closed

---
+++ src/components/enrollment/Enrollment.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Enrollment from './Enrollment'
+
+storiesOf('Layout', module)
+  .add('Enrollment', () => (
+    <Enrollment />
+  ))

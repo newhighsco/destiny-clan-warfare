5c94ff187adb87fec9f818e2b3298a7b065578e9
Ben Edwards <ben.edwards@clock.co.uk>
2018-04-06 09:31:53 +0100

    > Fixes some html errors

--- src/html.js
+++ src/html.js
@@ -6,7 +6,7 @@ const constants = require('./utils/constants')
 class Html extends Component {
   render () {
     const { Html, Head, Body, children, routeInfo } = this.props
-    const { title, name, description, handle, themeColor } = constants.meta
+    const { title, name, handle, themeColor } = constants.meta
     const canonicalPath = routeInfo && routeInfo.path !== '404' ? (routeInfo.path.match(/\/$/) ? routeInfo.path : `${routeInfo.path}/`) : null
     const canonicalUrl = `${process.env.SITE_URL}${canonicalPath === '/' ? canonicalPath : `/${canonicalPath}`}`
 
@@ -20,11 +20,9 @@ class Html extends Component {
           <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
           <link rel="manifest" href="/manifest.webmanifest" />
           <meta name="theme-color" content={themeColor} />
-          <meta name="description" content={description} />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content={name} />
           <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:domain" content={process.env.SITE_URL} />
           <meta name="twitter:site" content={handle} />
@@ -38,15 +36,15 @@ class Html extends Component {
         </Head>
         <Body>
           {children}
+          {constants.meta.trackingId &&
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '${constants.meta.trackingId}', 'auto');ga('send', 'pageview');`
+              }}
+            />
+          }
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
         </Body>
-        {constants.meta.trackingId &&
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '${constants.meta.trackingId}', 'auto');ga('send', 'pageview');`
-            }}
-          />
-        }
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
       </Html>
     )
   }

2da53112a255ab86a967f92492ce0eb3e99ddea7
Ben Edwards <ben.edwards@clock.co.uk>
2018-05-14 16:44:07 +0100

    Adds Branding guidelines page

--- src/components/prose/Prose.js
+++ src/components/prose/Prose.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './Prose.styl'
 
-const Prose = ({ children }) => {
+const Prose = ({ children, className }) => {
   return (
-    <div className={styles.prose}>
+    <div className={classNames(styles.prose, className)}>
       {children}
     </div>
   )
 }
 
 Prose.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  className: PropTypes.string
 }
 
 export default Prose

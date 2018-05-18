e0110411b6f6292f584826744c98b0fb12f0f0a3
Ben Edwards <ben.edwards@clock.co.uk>
2018-05-18 13:13:29 +0100

    Auto-redirect "Loading" pages

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,14 +1,18 @@
 import React, { PureComponent } from 'react'
 import { Head } from 'react-static'
+import NProgress from 'nprogress'
 import HoldingPage from '../holding-page/HoldingPage'
 import { Logo } from '../logo/Logo'
 
 class Loading extends PureComponent {
   render () {
+    NProgress.start()
+
     return (
       <HoldingPage>
         <Head>
           <title>Loading</title>
+          <meta httpEquiv="refresh" content="0" />
         </Head>
         <Logo />
       </HoldingPage>

1be69cc0f0f11abac652ece85a22bea0de086451
Ben Edwards <ben.edwards@clock.co.uk>
2018-05-22 18:05:37 +0100

    > Removes null values

--- src/utils/grammar.js
+++ src/utils/grammar.js
@@ -3,7 +3,7 @@ const possessive = (s) => {
 }
 
 const sentence = (a) => {
-  return a ? a.sort().join(', ').replace(/,(?!.*,)/g, ' &') : ''
+  return a ? a.filter(a => a).sort().join(', ').replace(/,(?!.*,)/g, ' &') : ''
 }
 
 module.exports = {

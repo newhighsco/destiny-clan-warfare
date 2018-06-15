9743810ebf0cd81000843c2cf3333be5e64dfd9a
Ben Edwards <ben.edwards@clock.co.uk>
2018-06-15 13:22:19 +0100

    > Rename

--- src/Html2.js
+++ src/Html2.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 const constants = require('./utils/constants')
+const { name, handle, themeColor } = constants.meta
 
-class Html extends Component {
+class Html extends PureComponent {
   render () {
     const { Html, Head, Body, children, routeInfo } = this.props
-    const { name, handle, themeColor } = constants.meta
     const canonicalPath = routeInfo && routeInfo.path !== '404' ? (routeInfo.path.match(/\/$/) ? routeInfo.path : `${routeInfo.path}/`) : null
     const canonicalUrl = `${process.env.SITE_URL}${canonicalPath === '/' ? canonicalPath : `/${canonicalPath}`}`
 

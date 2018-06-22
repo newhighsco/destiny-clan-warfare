17be510d115b133391df3249b6d873423e80d7b4
Ben Edwards <ben.edwards@clock.co.uk>
2018-06-22 19:25:37 +0100

    > Converts to PureComponent

--- src/components/platform/Platform.js
+++ src/components/platform/Platform.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import MultiSort from 'multi-sort'
@@ -9,35 +9,27 @@ import XboxSvg from './icons/xbox.svg'
 import styles from './Platform.styl'
 
 const baseClassName = 'platform'
+const icons = [
+  { id: 1, Svg: XboxSvg, name: 'Xbox Live' },
+  { id: 2, Svg: PlaystationSvg, name: 'PlayStation Network' },
+  { id: 4, Svg: BattlenetSvg, name: 'Battle.net' }
+]
 
-const Platform = ({ platform, size }) => {
-  var Svg
-  var name
+class Platform extends PureComponent {
+  render () {
+    const { platform, size } = this.props
+    const { Svg, name } = icons.find(({ id }) => id === platform.id)
 
-  switch (platform.id) {
-    case 1:
-      Svg = XboxSvg
-      name = 'Xbox Live'
-      break
-    case 2:
-      Svg = PlaystationSvg
-      name = 'PlayStation Network'
-      break
-    case 4:
-      Svg = BattlenetSvg
-      name = 'Battle.net'
-      break
+    if (!Svg) return null
+
+    return (
+      <div className={classNames(styles[baseClassName], size && styles[`${baseClassName}--${size}`])}>
+        <Icon a11yText={name} className={styles[`${baseClassName}__icon`]}>
+          <Svg />
+        </Icon>
+      </div>
+    )
   }
-
-  if (!Svg) return null
-
-  return (
-    <div className={classNames(styles[baseClassName], size && styles[`${baseClassName}--${size}`])}>
-      <Icon a11yText={name} className={styles[`${baseClassName}__icon`]}>
-        <Svg />
-      </Icon>
-    </div>
-  )
 }
 
 Platform.propTypes = {
@@ -45,29 +37,29 @@ Platform.propTypes = {
   size: PropTypes.oneOf([ 'small' ])
 }
 
-const PlatformList = ({ platforms, size, className }) => {
-  if (!platforms || platforms.length < 1) return null
+class PlatformList extends PureComponent {
+  render () {
+    const { size, className } = this.props
+    var { platforms } = this.props
 
-  platforms = MultiSort(platforms, {
-    size: 'DESC',
-    active: 'DESC',
-    id: 'ASC'
-  })
+    platforms = MultiSort(platforms.filter(platform => platform.percentage >= 10), {
+      size: 'DESC',
+      active: 'DESC',
+      id: 'ASC'
+    })
 
-  const totalSize = platforms.map(platform => platform.size).reduce((a, b) => a + b, 0)
+    if (!platforms || platforms.length < 1) return null
 
-  platforms.map(platform => { platform.percentage = Math.round((platform.size / totalSize) * 100) })
-  platforms = platforms.filter(platform => platform.percentage >= 10)
-
-  return (
-    <ul className={classNames('list--inline', styles[`${baseClassName}-list`], className)}>
-      {platforms.map((platform, i) => (
-        <li key={i}>
-          <Platform platform={platform} size={size} />
-        </li>
-      ))}
-    </ul>
-  )
+    return (
+      <ul className={classNames('list--inline', styles[`${baseClassName}-list`], className)}>
+        {platforms.map((platform, i) => (
+          <li key={i}>
+            <Platform platform={platform} size={size} />
+          </li>
+        ))}
+      </ul>
+    )
+  }
 }
 
 PlatformList.propTypes = {

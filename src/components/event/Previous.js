fbb58a8b331beb3c7f3e8a44e63d5c1ba06ebdb9
Ben Edwards <ben.edwards@clock.co.uk>
2018-06-22 19:18:55 +0100

    > Converts to PureComponent

--- src/components/event/Previous.js
+++ src/components/event/Previous.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Card from '../card/Card'
 import { Lockup } from '../lockup/Lockup'
@@ -13,57 +13,61 @@ import Prose from '../prose/Prose'
 
 const constants = require('../../utils/constants')
 
-const PreviousEvent = ({ event, element, summary }) => {
-  if (!event) return null
+class PreviousEvent extends PureComponent {
+  render () {
+    const { event, element, summary } = this.props
 
-  const leaderboards = event.leaderboards
-  const isCalculated = event.isCalculated && (leaderboards.length === constants.divisions.length)
+    if (!event) return null
 
-  return (
-    <Fragment>
-      <Card cutout={isCalculated} center>
-        <Lockup center element={element} headingHref={summary && event.path} heading={event.name} />
-        <Timer start={event.startDate} end={event.endDate} />
-        {event.description &&
-          <Prose>
-            <p dangerouslySetInnerHTML={{ __html: event.description }} />
-          </Prose>
+    const leaderboards = event.leaderboards
+    const isCalculated = event.isCalculated && (leaderboards.length === constants.divisions.length)
+
+    return (
+      <Fragment>
+        <Card cutout={isCalculated} center>
+          <Lockup center element={element} headingHref={summary && event.path} heading={event.name} />
+          <Timer start={event.startDate} end={event.endDate} />
+          {event.description &&
+            <Prose>
+              <p dangerouslySetInnerHTML={{ __html: event.description }} />
+            </Prose>
+          }
+          <ModifierList modifiers={event.modifiers} />
+          {!summary && isCalculated && event.medals &&
+            <Fragment>
+              <MedalList medals={event.medals.clans} kicker="Medals awarded" />
+              <MedalList medals={event.medals.members} />
+            </Fragment>
+          }
+          {!isCalculated &&
+            <Notification>Results for this event are being calculated. Please check back later.</Notification>
+          }
+          {isCalculated && summary &&
+            <Button href={`${event.path}#results`}>View full results</Button>
+          }
+        </Card>
+        {isCalculated &&
+          summary ? (
+            <TabContainer cutout>
+              <Tab name="Winners">
+                <Leaderboard data={event.results} />
+              </Tab>
+            </TabContainer>
+          ) : (
+            <TabContainer id="results" cutout>
+              {leaderboards.map(({ leaderboard, division }) => {
+                return (
+                  <Tab key={division.name} name={division.name} title={division.size}>
+                    <Leaderboard data={leaderboard} />
+                  </Tab>
+                )
+              })}
+            </TabContainer>
+          )
         }
-        <ModifierList modifiers={event.modifiers} />
-        {!summary && isCalculated && event.medals &&
-          <Fragment>
-            <MedalList medals={event.medals.clans} kicker="Medals awarded" />
-            <MedalList medals={event.medals.members} />
-          </Fragment>
-        }
-        {!isCalculated &&
-          <Notification>Results for this event are being calculated. Please check back later.</Notification>
-        }
-        {isCalculated && summary &&
-          <Button href={`${event.path}#results`}>View full results</Button>
-        }
-      </Card>
-      {isCalculated &&
-        summary ? (
-          <TabContainer cutout>
-            <Tab name="Winners">
-              <Leaderboard data={event.results} />
-            </Tab>
-          </TabContainer>
-        ) : (
-          <TabContainer id="results" cutout>
-            {leaderboards.map(({ leaderboard, division }) => {
-              return (
-                <Tab key={division} name={division}>
-                  <Leaderboard data={leaderboard} />
-                </Tab>
-              )
-            })}
-          </TabContainer>
-        )
-      }
-    </Fragment>
-  )
+      </Fragment>
+    )
+  }
 }
 
 PreviousEvent.defaultProps = {

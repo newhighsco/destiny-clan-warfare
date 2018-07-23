de0ed76e12a3ba0c69ac0103541459ded3270d87
Ben Edwards <ben.edwards@clock.co.uk>
2018-07-23 14:37:38 +0100

    > Point to dev API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -53,7 +53,7 @@ const constants = {
   server: {
     apiUrl: {
       protocol: 'https://',
-      subdomains: [ 'destinyclanwarfareapi' ],
+      subdomains: [ 'destinyclanwarfaredev' ],
       domain: '.azurewebsites.net/',
       path: 'api/'
     },

9be9e175119ee8b75d9c4c8b870b5733f61ec04f
Ben Edwards <ben.edwards@clock.co.uk>
2018-07-26 18:27:25 +0100

    > Adds participation level medals

--- src/components/medal/foreground.js
+++ src/components/medal/foreground.js
@@ -1,7 +1,10 @@
+import EverythingButTheKitchenSink from './foreground/everything-but-the-kitchen-sink.svg'
 import FirstCompany from './foreground/first-company.svg'
 import FirstPlatoon from './foreground/first-platoon.svg'
 import FirstSquad from './foreground/first-squad.svg'
+import FullHouse from './foreground/full-house.svg'
 import TheBestOfTheBest from './foreground/the-best-of-the-best.svg'
+import TheMoreYouGiveTheMoreYouGet from './foreground/the-more-you-give-the-more-you-get.svg'
 import TopTenCompany from './foreground/top-10-company.svg'
 import TopTenPlatoon from './foreground/top-10-platoon.svg'
 import TopTenSquad from './foreground/top-10-squad.svg'
@@ -10,10 +13,13 @@ import TopThreePlatoon from './foreground/top-3-platoon.svg'
 import TopThreeSquad from './foreground/top-3-squad.svg'
 
 const foregrounds = {
+  EverythingButTheKitchenSink: { svg: EverythingButTheKitchenSink },
   FirstCompany: { svg: FirstCompany },
   FirstPlatoon: { svg: FirstPlatoon },
   FirstSquad: { svg: FirstSquad },
+  FullHouse: { svg: FullHouse },
   TheBestOfTheBest: { svg: TheBestOfTheBest },
+  TheMoreYouGiveTheMoreYouGet: { svg: TheMoreYouGiveTheMoreYouGet },
   Top_10Company: { svg: TopTenCompany },
   Top_10Platoon: { svg: TopTenPlatoon },
   Top_10Squad: { svg: TopTenSquad },

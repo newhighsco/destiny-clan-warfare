2eac62b5d2de0b123dedbdb7e66739e81806de9d
Ben Edwards <ben.edwards@clock.co.uk>
2018-07-27 10:20:30 +0100

    > Adds medal size guide to storybook

--- src/components/medal/Medal.stories.js
+++ src/components/medal/Medal.stories.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { storiesOf } from '@storybook/react'
 import { MedalList } from './Medal'
@@ -6,6 +6,16 @@ import { MedalList } from './Medal'
 const proxy = require('../../utils/api-helper').proxy()
 const medalBuilder = require('../../utils/medal-builder')
 const constants = require('../../utils/constants')
+const medals = [
+  { name: 'Top 3 company', tier: 1, count: 1 },
+  { name: 'Top 3 company', tier: 1, count: 5 },
+  { name: 'First company', tier: 2, count: 1 },
+  { name: 'First company', tier: 2, count: 5 },
+  { name: 'The best of the best', tier: 3, count: 1 },
+  { name: 'The best of the best', tier: 3, count: 5 },
+  { name: 'Z Nonexistent', tier: 1, count: 5 },
+  { name: 'Z Nonexistent', tier: 1, count: 1 }
+]
 
 class Loader extends PureComponent {
   constructor (props) {
@@ -35,18 +45,27 @@ Loader.propTypes = {
 }
 
 storiesOf('Medals', module)
-  .addDecorator(story => (
+  .add('Clan', () => (
     <div className="storybook-tooltips-visible">
-      {story()}
+      <Loader type={constants.prefix.clan} url="Component/GetAllClanMedals">
+        {medals => <MedalList medals={medals} enableHover={false} tooltipActive />}
+      </Loader>
     </div>
   ))
-  .add('Clan', () => (
-    <Loader type={constants.prefix.clan} url="Component/GetAllClanMedals">
-      {medals => <MedalList medals={medals} enableHover={false} tooltipActive />}
-    </Loader>
-  ))
   .add('Members', () => (
-    <Loader type={constants.prefix.profile} url="Component/GetAllMedals">
-      {medals => <MedalList medals={medals} enableHover={false} tooltipActive />}
-    </Loader>
+    <div className="storybook-tooltips-visible">
+      <Loader type={constants.prefix.profile} url="Component/GetAllMedals">
+        {medals => <MedalList medals={medals} enableHover={false} tooltipActive />}
+      </Loader>
+    </div>
+  ))
+  .add('Sizes', () => (
+    <Fragment>
+      <p>Regular</p>
+      <MedalList medals={medals} enableHover={false} />
+      <p>Small</p>
+      <MedalList medals={medals} enableHover={false} size="small" />
+      <p>X-Small</p>
+      <MedalList medals={medals} enableHover={false} size="x-small" />
+    </Fragment>
   ))

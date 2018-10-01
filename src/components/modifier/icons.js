0558c17b0bf9e75c7991cf2801aba047a86cba53
Ben Edwards <ben.edwards@clock.co.uk>
2018-10-01 12:29:54 +0100

    Adds missing modifier icons

--- src/components/modifier/icons.js
+++ src/components/modifier/icons.js
@@ -1,12 +1,16 @@
 import AbuseOfPower from './icons/abuse-of-power.svg'
+import AllCreaturesGreatAndSmall from './icons/all-creatures-great-and-small.svg'
 import AlwaysByYourSide from './icons/always-by-your-side.svg'
 import AntiMeta from './icons/anti-meta.svg'
 import AnythingYouCanDo from './icons/anything-you-can-do.svg'
 import AutomaticLockdown from './icons/automatic-lockdown.svg'
+import AvengeCayde6 from './icons/avenge-cayde-6.svg'
 import BandTogether from './icons/band-together.svg'
 import BeASuperhero from './icons/be-a-superhero.svg'
 import BoyScout from './icons/boy-scout.svg'
 import Bullseye from './icons/bullseye.svg'
+import CleanSweep from './icons/clean-sweep.svg'
+import CompleteTheCycle from './icons/complete-the-cycle.svg'
 import CrimsonDoubleDown from './icons/crimson-double-down.svg'
 import Deathless from './icons/deathless.svg'
 import DoubleDown from './icons/double-down.svg'
@@ -18,12 +22,15 @@ import FragAndTag from './icons/frag-and-tag.svg'
 import HearTheWhisper from './icons/hear-the-whisper.svg'
 import HelpingHand from './icons/helping-hand.svg'
 import IHitARock from './icons/i-hit-a-rock.svg'
+import LetMeHandleThis from './icons/let-me-handle-this.svg'
 import LukeSkywalkersPadawan from './icons/luke-skywalkers-padawan.svg'
+import MakingADeposit from './icons/making-a-deposit.svg'
 import Merciless from './icons/merciless.svg'
 import NiceThrow from './icons/nice-throw.svg'
 import NoApologies from './icons/no-apologies.svg'
 import NotSoSuper from './icons/not-so-super.svg'
 import NotVeryExotic from './icons/not-very-exotic.svg'
+import OhNoYouDont from './icons/oh-no-you-dont.svg'
 import Perfection from './icons/perfection.svg'
 import PewPewPew from './icons/pew-pew-pew.svg'
 import PowerHungry from './icons/power-hungry.svg'
@@ -44,14 +51,18 @@ import ZoneCaptured from './icons/zone-captured.svg'
 const svgs = {
   '20_20': { svg: TwentyTwenty, designer: 'Chanut is Industries' },
   AbuseOfPower: { svg: AbuseOfPower, designer: 'naim' },
+  AllCreaturesGreatSmall: { svg: AllCreaturesGreatAndSmall },
   AlwaysByYourSide: { svg: AlwaysByYourSide, designer: 'Simon Child' },
   AntiMeta: { svg: AntiMeta, designer: 'TMD' },
   AnythingYouCanDo: { svg: AnythingYouCanDo },
   AutomaticLockdown: { svg: AutomaticLockdown, designer: 'Jaime M. Laurel' },
+  AvengeCayde_6: { svg: AvengeCayde6, designer: 'farias' },
   BandTogether: { svg: BandTogether, designer: 'Gabriel Vogel' },
   BeASuperhero: { svg: BeASuperhero },
   BoyScout: { svg: BoyScout, designer: 'AomAm' },
   Bullseye: { svg: Bullseye, designer: 'Zlatko Najdenovski' },
+  CleanSweep: { svg: CleanSweep, designer: 'Rudez Studio' },
+  CompleteTheCycle: { svg: CompleteTheCycle, designer: 'Gregor Cresnar' },
   CrimsonDoubleDown: { svg: CrimsonDoubleDown, designer: 'Ker\'is & Optimus Prime' },
   Deathless: { svg: Deathless, designer: 'Robert Bjurshagen' },
   DoubleDown: { svg: DoubleDown, designer: 'Optimus Prime' },
@@ -65,12 +76,16 @@ const svgs = {
   HearTheWhisper: { svg: HearTheWhisper, designer: 'BGBOXXX Design' },
   HelpingHand: { svg: HelpingHand, designer: 'Demetria Rose' },
   IHitARock: { svg: IHitARock, designer: 'Tom Fricker' },
+  LetMeHandleThis: { svg: LetMeHandleThis },
   LukeSkywalkerSPadawan: { svg: LukeSkywalkersPadawan, designer: 'Colour Block' },
+  MakingADeposit: { svg: MakingADeposit },
+  MakeTheDreamwork: { svg: BandTogether, designer: 'Gabriel Vogel' },
   Merciless: { svg: Merciless, designer: 'Optimus Prime' },
   NiceThrow: { svg: NiceThrow, designer: 'Hea Poh Lin' },
   NoApologies: { svg: NoApologies, designer: 'Simon Child' },
   NotSoSuper: { svg: NotSoSuper },
   NotVeryExotic: { svg: NotVeryExotic, designer: 'iconsphere' },
+  OhNoYouDonT: { svg: OhNoYouDont },
   Perfection: { svg: Perfection, designer: 'Montu Yadav' },
   PewPewPew: { svg: PewPewPew, designer: 'Nikol Kokešová' },
   PowerHungry: { svg: PowerHungry, designer: 'naim' },

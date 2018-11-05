44291be198a513101db922df650d239055c7f7f4
Ben Edwards <ben.edwards@clock.co.uk>
2018-11-05 10:50:19 +0000

    Adds Gambit clan medals

--- src/components/medal/foreground.js
+++ src/components/medal/foreground.js
@@ -1,23 +1,29 @@
-import EverythingButTheKitchenSink from './foreground/everything-but-the-kitchen-sink.svg'
-import FirstCompany from './foreground/first-company.svg'
-import FirstPlatoon from './foreground/first-platoon.svg'
-import FirstSquad from './foreground/first-squad.svg'
-import FullHouse from './foreground/full-house.svg'
-import TheBestOfTheBest from './foreground/the-best-of-the-best.svg'
-import TheMoreYouGiveTheMoreYouGet from './foreground/the-more-you-give-the-more-you-get.svg'
-import TopTenCompany from './foreground/top-10-company.svg'
-import TopTenPlatoon from './foreground/top-10-platoon.svg'
-import TopTenSquad from './foreground/top-10-squad.svg'
-import TopThreeCompany from './foreground/top-3-company.svg'
-import TopThreePlatoon from './foreground/top-3-platoon.svg'
-import TopThreeSquad from './foreground/top-3-squad.svg'
+import EverythingButTheKitchenSink from './foreground/clan/everything-but-the-kitchen-sink.svg'
+import FirstCompany from './foreground/clan/first-company.svg'
+import FirstPlatoon from './foreground/clan/first-platoon.svg'
+import FirstSquad from './foreground/clan/first-squad.svg'
+import FullHouse from './foreground/clan/full-house.svg'
+import GambitChampionsCompany from './foreground/clan/gambit-champions-company.svg'
+import GambitChampionsPlatoon from './foreground/clan/gambit-champions-platoon.svg'
+import GambitChampionsSquad from './foreground/clan/gambit-champions-squad.svg'
+import TheBestOfTheBest from './foreground/clan/the-best-of-the-best.svg'
+import TheMoreYouGiveTheMoreYouGet from './foreground/clan/the-more-you-give-the-more-you-get.svg'
+import TopTenCompany from './foreground/clan/top-10-company.svg'
+import TopTenPlatoon from './foreground/clan/top-10-platoon.svg'
+import TopTenSquad from './foreground/clan/top-10-squad.svg'
+import TopThreeCompany from './foreground/clan/top-3-company.svg'
+import TopThreePlatoon from './foreground/clan/top-3-platoon.svg'
+import TopThreeSquad from './foreground/clan/top-3-squad.svg'
 
-const foregrounds = {
+const clan = {
   EverythingButTheKitchenSink: { svg: EverythingButTheKitchenSink },
   FirstCompany: { svg: FirstCompany },
   FirstPlatoon: { svg: FirstPlatoon },
   FirstSquad: { svg: FirstSquad },
   FullHouse: { svg: FullHouse },
+  InauguralGambitChampions: { svg: GambitChampionsCompany },
+  InauguralGambitPlatoonChampions: { svg: GambitChampionsPlatoon },
+  InauguralGambitSquadChampions: { svg: GambitChampionsSquad },
   TheBestOfTheBest: { svg: TheBestOfTheBest },
   TheMoreYouGiveTheMoreYouGet: { svg: TheMoreYouGiveTheMoreYouGet },
   Top_10Company: { svg: TopTenCompany },
@@ -28,4 +34,7 @@ const foregrounds = {
   Top_3Squad: { svg: TopThreeSquad }
 }
 
-export default foregrounds
+const member = {
+}
+
+export default { ...clan, ...member }

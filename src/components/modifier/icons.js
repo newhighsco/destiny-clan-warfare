0749be37165b7a470ca85860ba46458cbda43062
Ben Edwards <ben.edwards@clock.co.uk>
2018-11-05 09:53:19 +0000

    Adds missing modifiers

--- src/components/modifier/icons.js
+++ src/components/modifier/icons.js
@@ -7,6 +7,7 @@ import AutomaticLockdown from './icons/automatic-lockdown.svg'
 import AvengeCayde6 from './icons/avenge-cayde-6.svg'
 import BandTogether from './icons/band-together.svg'
 import BeASuperhero from './icons/be-a-superhero.svg'
+import BownessPoints from './icons/bowness-points.svg'
 import BoyScout from './icons/boy-scout.svg'
 import Bullseye from './icons/bullseye.svg'
 import CleanSweep from './icons/clean-sweep.svg'
@@ -31,6 +32,7 @@ import NoApologies from './icons/no-apologies.svg'
 import NotSoSuper from './icons/not-so-super.svg'
 import NotVeryExotic from './icons/not-very-exotic.svg'
 import OhNoYouDont from './icons/oh-no-you-dont.svg'
+import OopsImTrash from './icons/oops-im-trash.svg'
 import Perfection from './icons/perfection.svg'
 import PewPewPew from './icons/pew-pew-pew.svg'
 import PowerHungry from './icons/power-hungry.svg'
@@ -59,8 +61,10 @@ const svgs = {
   AvengeCayde_6: { svg: AvengeCayde6, designer: 'farias' },
   BandTogether: { svg: BandTogether, designer: 'Gabriel Vogel' },
   BeASuperhero: { svg: BeASuperhero },
+  BowNessPoints: { svg: BownessPoints, designer: 'Hat-Tech' },
   BoyScout: { svg: BoyScout, designer: 'AomAm' },
   Bullseye: { svg: Bullseye, designer: 'Zlatko Najdenovski' },
+  ChillAtHome: { svg: BandTogether, designer: 'Gabriel Vogel' },
   CleanSweep: { svg: CleanSweep, designer: 'Rudez Studio' },
   CompleteTheCycle: { svg: CompleteTheCycle, designer: 'Gregor Cresnar' },
   CrimsonDoubleDown: { svg: CrimsonDoubleDown, designer: 'Ker\'is & Optimus Prime' },
@@ -80,12 +84,14 @@ const svgs = {
   LukeSkywalkerSPadawan: { svg: LukeSkywalkersPadawan, designer: 'Colour Block' },
   MakingADeposit: { svg: MakingADeposit },
   MakeTheDreamwork: { svg: BandTogether, designer: 'Gabriel Vogel' },
+  MyMotesBringAllTheBoysToTheYard: { svg: AllCreaturesGreatAndSmall },
   Merciless: { svg: Merciless, designer: 'Optimus Prime' },
   NiceThrow: { svg: NiceThrow, designer: 'Hea Poh Lin' },
   NoApologies: { svg: NoApologies, designer: 'Simon Child' },
   NotSoSuper: { svg: NotSoSuper },
   NotVeryExotic: { svg: NotVeryExotic, designer: 'iconsphere' },
   OhNoYouDonT: { svg: OhNoYouDont },
+  OopsIMTrash: { svg: OopsImTrash },
   Perfection: { svg: Perfection, designer: 'Montu Yadav' },
   PewPewPew: { svg: PewPewPew, designer: 'Nikol Kokešová' },
   PowerHungry: { svg: PowerHungry, designer: 'naim' },

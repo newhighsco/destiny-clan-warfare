3ce82566d1435c97b5fdb5db209dcacc47b1c3c7
Ben Edwards <ben.edwards@clock.co.uk>
2019-02-11 14:01:05 +0000

    Adds Percy for visual regression testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Router } from 'react-static'
-import { configure, addDecorator } from '@storybook/react'
+import { configure, addDecorator, getStorybook, setAddon } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
+import createPercyAddon from '@percy-io/percy-storybook'
+import inPercy from '@percy-io/in-percy'
+import mockdate from 'mockdate'
 
 import '../src/stylus/index.styl'
 import './storybook.styl'
 
+if (inPercy()) {
+  mockdate.set('October 21, 2015 04:19:00');
+}
+
 const constants = require('../src/utils/constants')
 const req = require.context('../src/components', true, /\.stories\.js$/)
+const { percyAddon, serializeStories } = createPercyAddon()
 
 function loadStories () {
   req.keys().forEach((filename) => req(filename))
@@ -21,10 +29,14 @@ addDecorator(story => (
   </div>
 ))
 
+setAddon(percyAddon)
+
+configure(loadStories, module)
+
+serializeStories(getStorybook)
+
 setOptions({
   name: constants.meta.name,
   url: '/',
   showAddonPanel: false
 })
-
-configure(loadStories, module)

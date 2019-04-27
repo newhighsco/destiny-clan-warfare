dd7e4928ccf826bcb3f35041068222044da7da92
Ben Edwards <ben.edwards@clock.co.uk>
2019-04-27 14:41:46 +0100

    > Fixes broken plugin

--- plugins/plugin-webpack/node.api.js
+++ plugins/plugin-webpack/node.api.js
@@ -3,10 +3,13 @@ import webpack from 'webpack'
 export default () => ({
   webpack: config => {
     const existingUrlLoaderRule = config.module.rules[0].oneOf.findIndex(
-      ({ loader, query }) => loader === 'url-loader' && query && query.limit
+      ({ loader, query }) => loader === 'url-loader'
     )
 
-    config.module.rules[0].oneOf[existingUrlLoaderRule].query.limit = 1
+    config.module.rules[0].oneOf[existingUrlLoaderRule].query = {
+      limit: 1,
+      name: 'static/[name].[hash:8].[ext]'
+    }
 
     config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
 

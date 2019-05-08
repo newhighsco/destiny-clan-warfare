0a889b5727059d690b7993802d047276acfdd9fe
Ben Edwards <ben.edwards@clock.co.uk>
2019-05-08 13:54:45 +0100

    > Removes broken poly-fill

--- plugins/plugin-webpack/node.api.js
+++ plugins/plugin-webpack/node.api.js
@@ -2,8 +2,6 @@ import webpack from 'webpack'
 
 export default () => ({
   webpack: (config, { defaultLoaders, stage }) => {
-    config.entry = [require.resolve('babel-polyfill'), ...config.entry]
-
     const { fileLoader } = defaultLoaders
     const existingUrlLoaderRule = config.module.rules[0].oneOf.findIndex(
       ({ loader, query }) => loader === 'url-loader'

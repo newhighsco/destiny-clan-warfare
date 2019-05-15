843f9b6b054c6c5fa25d41f445d441587c2d7e14
Ben Edwards <ben.edwards@clock.co.uk>
2019-05-15 21:27:52 +0100

    Matches Storybook grid size to match Stylus spacing unit

--- .storybook/theme/index.js
+++ .storybook/theme/index.js
@@ -5,5 +5,6 @@ const constants = require('../../src/utils/constants')
 export default create({
   base: 'light',
   brandTitle: constants.meta.name,
-  brandImage: null
+  brandImage: null,
+  gridCellSize: 20
 })

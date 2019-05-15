d91c781611ec948e37c9dd4387035e85d19d65d3
Ben Edwards <ben.edwards@clock.co.uk>
2019-05-15 21:31:59 +0100

    > Enables Storybook source addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,6 +10,11 @@ module.exports = async ({ config }) => {
 
   config.module.rules.push(
     {
+      test: /\.stories\.jsx?$/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre'
+    },
+    {
       test: /\.styl$/,
       use: [...stylusLoaders('dev')]
     },

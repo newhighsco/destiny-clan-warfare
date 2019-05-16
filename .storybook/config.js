8ebda4e8ec7cb6b87c6cde17ac6ff4d689b4db08
Ben Edwards <ben.edwards@clock.co.uk>
2019-05-16 07:33:03 +0100

    > Disables Storybook info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { Router } from '@reach/router'
 import { addDecorator, addParameters, configure } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
-import { withInfo } from '@storybook/addon-info'
 import inPercy from '@percy-io/in-percy'
 import mockdate from 'mockdate'
 import theme from './theme'
@@ -34,40 +33,6 @@ addParameters({
     theme
   }
 })
-
-addDecorator(
-  withInfo({
-    inline: false,
-    styles: {
-      button: {
-        topRight: {
-          borderRadius: '5px 0 0 0',
-          bottom: 0,
-          top: 'auto',
-          zIndex: 99998,
-          backgroundColor: '#1565c0'
-        }
-      },
-      infoStory: {
-        backgroundColor: 'rgb(255, 255, 255)',
-        backgroundImage:
-          'linear-gradient(to bottom, rgb(238, 238, 238) 0px, rgb(238, 238, 238) 1px, rgb(250, 250, 250) 1px, rgb(250, 250, 250) calc(100% - 1px), rgb(238, 238, 238) calc(100% - 1px), rgb(238, 238, 238) 100%)',
-        backgroundPosition: '50% 50%',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'calc(100% - 80px) 100%',
-        padding: '20px 60px'
-      },
-      infoBody: {
-        border: 0,
-        margin: 0,
-        paddingBottom: 20
-      },
-      infoContent: {
-        counterReset: 'selector-hack--story-info-content'
-      }
-    }
-  })
-)
 
 addDecorator(withA11y)
 

600f96edda431dd216474ffd4b47a00cef025886
Ben Edwards <hello@benedfit.com>
2019-07-30 11:19:20 +0100

    Maps Steam and Google Stadia to correct BungieMembershipType IDs
    https://bungie-net.github.io/multi/schema_BungieMembershipType.html#schema_BungieMembershipType

--- src/components/platform/platforms.js
+++ src/components/platform/platforms.js
@@ -1,9 +1,9 @@
 const platforms = [
   { id: 1, name: 'Xbox Live' },
   { id: 2, name: 'PlayStation Network' },
+  { id: 3, name: 'Steam' },
   { id: 4, name: 'Battle.net' },
-  { id: 'xxx', name: 'Google Stadia' },
-  { id: 'yyy', name: 'Steam' }
+  { id: 5, name: 'Google Stadia' }
 ]
 
 export default platforms

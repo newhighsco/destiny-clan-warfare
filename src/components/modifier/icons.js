efa265756d99350ac1feae997cdb28c76578a1c2
Ben Edwards <hello@benedfit.com>
2019-08-06 16:27:48 +0100

    Adds missing modifier icons

--- src/components/modifier/icons.js
+++ src/components/modifier/icons.js
@@ -1,14 +1,21 @@
 const antiMeta = { svg: 'anti-meta', designer: 'TMD' }
+const beASuperhero = { svg: 'be-a-superhero' }
 const bandTogether = { svg: 'band-together', designer: 'Gabriel Vogel' }
+const bullseye = { svg: 'bullseye', designer: 'Zlatko Najdenovski' }
 const crimsonDoubleDown = {
   svg: 'crimson-double-down',
   designer: "Ker'is & Optimus Prime"
 }
-const raidTiming = { svg: 'raid-timing', designer: 'Pixel Lab' }
-const bullseye = { svg: 'bullseye', designer: 'Zlatko Najdenovski' }
+const deathless = { svg: 'deathless', designer: 'Robert Bjurshagen' }
+const endAllExistence = {
+  svg: 'end-all-existence',
+  designer: 'Maciej Świerczek'
+}
+const elementary = { svg: 'elementary' }
+const modifierTBC = { svg: 'modifier-tbc' }
 const pinnacleKills = { svg: 'pinnacle-kills', designer: 'Creative Stall' }
 const pulsating = { svg: 'pulsating', designer: 'Nikol Kokešová' }
-const modifierTBC = { svg: 'modifier-tbc' }
+const raidTiming = { svg: 'raid-timing', designer: 'Pixel Lab' }
 
 const icons = {
   '20-20': { svg: 'twenty-twenty', designer: 'Chanut is Industries' },
@@ -18,6 +25,7 @@ const icons = {
   'automatic-lockdown': { designer: 'Jaime M. Laurel' },
   'avenge-cayde-6': { designer: 'farias' },
   'band-together': bandTogether,
+  'be-a-superhero': beASuperhero,
   'bow-ness-points': { designer: 'Hat-Tech' },
   'boy-scout': { designer: 'AomAm' },
   bullseye,
@@ -25,9 +33,11 @@ const icons = {
   'clean-sweep': { designer: 'Rudez Studio' },
   'complete-the-cycle': { designer: 'Gregor Cresnar' },
   'crimson-double-down': crimsonDoubleDown,
-  deathless: { designer: 'Robert Bjurshagen' },
+  deathless,
   'double-down': { designer: 'Optimus Prime' },
   'doubles-double-down': crimsonDoubleDown,
+  'end-all-existence': endAllExistence,
+  elementary,
   'feeling-blue': { designer: 'rivercon' },
   'flawless-striker': { designer: 'iconsphere' },
   'frag-and-tag': { designer: 'Hea Poh Lin, Krish & miza bin' },
@@ -36,20 +46,22 @@ const icons = {
   'hear-the-whisper': { designer: 'BGBOXXX Design' },
   'helping-hand': { designer: 'Demetria Rose' },
   'i-hit-a-rock': { designer: 'Tom Fricker' },
+  'kiss-from-a-row': { designer: 'estuhartono' },
   'luke-skywalker-s-padawan': { designer: 'Colour Block' },
   'make-the-dreamwork': bandTogether,
   'my-motes-bring-all-the-boys-to-the-yard': {
     svg: 'all-creatures-great-small'
   },
   merciless: { designer: 'Optimus Prime' },
-  modifierTBC,
+  'modifier-tbc': modifierTBC,
   'multiplier-tbc': modifierTBC,
   'nice-throw': { designer: 'Hea Poh Lin' },
   'no-apologies': { designer: 'Simon Child' },
   'not-very-exotic': { designer: 'iconsphere' },
   perfection: { designer: 'Montu Yadav' },
   'pew-pew-pew': pulsating,
-  pinnacleKills,
+  'pinaccle-kills': pinnacleKills,
+  'playing-cautiously': deathless,
   'power-hungry': { designer: 'naim' },
   pulsating,
   'raid-master': { designer: 'Robert Bjurshagen' },
@@ -59,7 +71,9 @@ const icons = {
   'shape-the-battlefield': modifierTBC,
   'stop-the-stopper': { designer: 'Optimus Prime' },
   'strike-death-penalty': { designer: 'Robert Bjurshagen' },
+  solstice: elementary,
   'super-accurate': bullseye,
+  superman: beASuperhero,
   'take-it-easy': { designer: 'Pixel Lab' },
   'telesto-festo': { designer: 'Casper Jensen' },
   'this-is-my-house': { designer: 'arif fajar yulianto' },

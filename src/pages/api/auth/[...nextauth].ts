513381209dd2a592596bd835d65574cfb5dc634b
Ben Edwards <hello@benedfit.com>
2021-10-28 09:17:49 +0100

    Debug

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-import { Session } from '@helpers/auth'
+// import { Session } from '@helpers/auth'
 import { signInUrl, signOutUrl } from '@helpers/urls'
-import { getMemberClans } from '@libs/bungie'
+// import { getMemberClans } from '@libs/bungie'
 // import { GroupV2Card } from 'bungie-api-ts/groupv2'
 
 export default NextAuth({
@@ -19,27 +19,31 @@ export default NextAuth({
   jwt: {
     signingKey: process.env.NEXTAUTH_JWT_SIGNING_KEY
   },
-  callbacks: {
-    jwt: async (token, user, account) => {
-      if (account) {
-        const clans = await getMemberClans(account.id)
+  // callbacks: {
+  //   jwt: async (token, user, account) => {
+  //     if (account) {
+  //       try {
+  //         const clans = await getMemberClans(account.id)
 
-        token.clans = clans.results.map(({ group: { groupId, name } }) => ({
-          groupId,
-          name
-        }))
-      }
+  //         token.clans = clans.results.map(({ group: { groupId, name } }) => ({
+  //           groupId,
+  //           name
+  //         }))
+  //       } catch {
+  //         token.clans = []
+  //       }
+  //     }
 
-      return Promise.resolve(token)
-    },
-    session: async (session, token) => {
-      const userSession = session as Session
-      userSession.user.membershipId = token.sub as string
-      userSession.user.clans = token.clans as Array<any>
+  //     return Promise.resolve(token)
+  //   },
+  //   session: async (session, token) => {
+  //     const userSession = session as Session
+  //     userSession.user.membershipId = token.sub as string
+  //     userSession.user.clans = token.clans as Array<GroupV2Card>
 
-      return Promise.resolve(userSession)
-    }
-  },
+  //     return Promise.resolve(userSession)
+  //   }
+  // },
   pages: {
     signIn: signInUrl,
     signOut: signOutUrl,

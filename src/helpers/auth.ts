2629d7284f0efdbe14927896902645f46fac7328
Ben Edwards <hello@benedfit.com>
2021-10-28 09:09:21 +0100

    debug

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,9 +1,9 @@
 import { DefaultSession } from 'next-auth'
-import { GroupV2Card } from 'bungie-api-ts/groupv2'
+// import { GroupV2Card } from 'bungie-api-ts/groupv2'
 
 export type Session = DefaultSession & {
   user: {
     membershipId: string
-    clans?: Array<GroupV2Card>
+    clans?: Array<any>
   }
 }

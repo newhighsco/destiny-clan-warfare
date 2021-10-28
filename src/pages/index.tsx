b7dc79844b666acfa9286bfb7c6b16c3fece0852
Ben Edwards <hello@benedfit.com>
2021-10-28 09:45:58 +0100

    Homepage WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import Link from 'next/link'
 import { LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
-import { canonicalUrl } from '@helpers/urls'
+import { Button, Card, Grid } from '@newhighsco/chipset'
+import { canonicalUrl, eventUrl } from '@helpers/urls'
 import PageContainer from '@components/PageContainer'
 import config from '@config'
+import Lockup from '@components/Lockup'
 
 const { logo, name, socialLinks, title, url } = config
 
 const HomePage: React.FC = ({
   meta
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const currentEvent = {
+    id: '1',
+    name: 'The name of the event',
+    description: 'Current event'
+  }
+
   return (
     <PageContainer meta={meta}>
       <SocialProfileJsonLd
@@ -21,6 +30,46 @@ const HomePage: React.FC = ({
       {logo?.bitmap && (
         <LogoJsonLd url={url} logo={canonicalUrl(logo.bitmap)} />
       )}
+      <Grid>
+        <Grid.Item sizes={['one-quarter']}>Previous event</Grid.Item>
+        <Grid.Item sizes={['one-half']}>
+          <Lockup
+            kicker="Current event"
+            kickerAttributes={{ as: 'h1' }}
+            align="center"
+            highlight
+          />
+          <br />
+          <Card
+            heading={
+              <Lockup
+                heading={currentEvent.name}
+                headingAttributes={{
+                  as: 'h2',
+                  href: eventUrl(currentEvent.id)
+                }}
+                align="center"
+              />
+            }
+            align="center"
+          >
+            {currentEvent.description}
+            <Button.Group>
+              <Link href={eventUrl(currentEvent.id)} passHref>
+                <Button>View...</Button>
+              </Link>
+            </Button.Group>
+          </Card>
+        </Grid.Item>
+        <Grid.Item sizes={['one-quarter']}>Next event</Grid.Item>
+        <Grid.Item align="center">
+          <Button.Group>
+            <Link href={eventUrl()} passHref>
+              <Button>View all events</Button>
+            </Link>
+          </Button.Group>
+        </Grid.Item>
+      </Grid>
     </PageContainer>
   )
 }

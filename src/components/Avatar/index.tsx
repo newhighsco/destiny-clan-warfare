872f672aafe8b27fd44d1543b1fae9819f320ff1
Ben Edwards <hello@benedfit.com>
2022-10-28 13:11:48 +0100

    chore(avatar): improve render

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -12,41 +12,68 @@ export enum AvatarSize {
   Medium = 'medium'
 }
 
-interface AvatarLayerProps {
-  id: string
+interface Layer {
   fill?: string
   src?: string
 }
 
-const AvatarLayer: React.FC<AvatarLayerProps> = ({ id, fill, src }) => {
-  if (!fill || !src) return null
+interface AvatarLayerProps {
+  id: number
+  layers: Layer[]
+  width?: number
+  height?: number
+  viewBox?: React.SVGProps<SVGSVGElement>['viewBox']
+}
 
-  const hex = hexToRgb(fill)
-  const r = round(hex.r / 255, 3)
-  const g = round(hex.g / 255, 3)
-  const b = round(hex.b / 255, 3)
+const AvatarLayer: React.FC<AvatarLayerProps> = ({
+  id,
+  layers,
+  width = '100%',
+  height = width,
+  viewBox = '0 0 1 1'
+}) => {
+  const defs = []
+  const images = []
+
+  layers.forEach(({ fill, src }, index) => {
+    if (fill && src) {
+      const [r, g, b] = hexToRgb(fill).map(v => round(v / 255, 3))
+      const key = [id, index].join('-')
+      const anchor = [id, r, g, b].join('-')
+
+      defs.push(
+        <filter key={key} id={anchor} x={0} y={0} width={width} height={height}>
+          <feColorMatrix
+            values={`${r} 0 0 0 0 0 ${g} 0 0 0 0 0 ${b} 0 0 0 0 0 1 0`}
+          />
+        </filter>
+      )
+
+      images.push(
+        <image
+          key={key}
+          width={width}
+          height={height}
+          filter={`url(#${anchor})`}
+          xlinkHref={src}
+        />
+      )
+    }
+  })
+
+  if (!images.length) return null
 
   return (
-    <svg className={styles.layer} viewBox="0 0 1 1">
-      <filter id={id} x="0" y="0" width="100%" height="100%">
-        <feColorMatrix
-          values={`${r} 0 0 0 0 0 ${g} 0 0 0 0 0 ${b} 0 0 0 0 0 1 0`}
-        />
-      </filter>
-      <image
-        width="100%"
-        height="100%"
-        filter={`url(#${id})`}
-        xlinkHref={src}
-      />
+    <svg className={styles.layer} viewBox={viewBox}>
+      <defs>{defs.map(def => def)}</defs>
+      <g>{images.map(image => image)}</g>
     </svg>
   )
 }
 
-interface AvatarProps extends Pick<AvatarLayerProps, 'fill' | 'src'> {
-  id?: number
-  background?: Omit<AvatarLayerProps, 'id'>
-  foreground?: Omit<AvatarLayerProps, 'id'>
+interface AvatarProps extends Partial<Pick<AvatarLayerProps, 'id'>>, Layer {
+  background?: Layer
+  foreground?: Layer
   size?: AvatarSize
   outline?: boolean
   align?: AlignSetting
@@ -56,7 +83,7 @@ interface AvatarProps extends Pick<AvatarLayerProps, 'fill' | 'src'> {
 
 const Avatar: React.FC<AvatarProps> = ({
   src = 'https://www.bungie.net/img/profile/avatars/default_avatar.gif',
-  fill,
+  fill: backgroundColor,
   background,
   foreground,
   size,
@@ -66,7 +93,7 @@ const Avatar: React.FC<AvatarProps> = ({
   className,
   children
 }) => {
-  const hasLayers = background?.src || foreground?.src
+  const hasLayers = background || foreground
 
   return (
     <div
@@ -78,13 +105,13 @@ const Avatar: React.FC<AvatarProps> = ({
         align && styles[align],
         className
       )}
-      style={{ backgroundColor: fill }}
+      style={{ backgroundColor }}
     >
       {hasLayers ? (
-        <>
-          {background && <AvatarLayer {...background} id={`${id}-bg`} />}
-          {foreground && <AvatarLayer {...foreground} id={`${id}-fg`} />}
-        </>
+        <AvatarLayer
+          id={id}
+          layers={[background, foreground].filter(Boolean)}
+        />
       ) : (
         children || <Image src={src} alt="" fill sizes={sizes.large} />
       )}
--- src/helpers/hex.ts
+++ src/helpers/hex.ts
@@ -2,20 +2,11 @@ export const isHex = (hex: string): RegExpExecArray => {
   return /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
 }
 
-export const hexToRgb = (
-  hex: string
-): {
-  r: number
-  g: number
-  b: number
-} => {
-  const result = isHex(hex)
+export const hexToRgb = (hex: string): [r: number, g: number, b: number] => {
+  const result = isHex(hex) || []
+  const [, r = 255, g = 255, b = 255] = result.map((value: string) =>
+    parseInt(value, 16)
+  )
 
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      }
-    : { r: 255, g: 255, b: 255 }
+  return [r, g, b]
 }

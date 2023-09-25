4348b667bdc6b2a2df2946f951e614279277209c
Ben Edwards <hello@benedfit.com>
2023-09-25 13:48:36 +0100

    fix typo

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'cypress'
 
+const DOMAIN = 'destiny-clan-warfare.netlify.app'
+
+const baseUrl = (): string => {
+  if (process.env.CIRCLE_PULL_REQUEST) {
+    return `https://deploy-preview-${process.env.CIRCLE_PR_NUMBER.split(
+      '/'
+    ).pop()}--${DOMAIN}`
+  }
+
+  if (process.env.CIRCLE_BRANCH) {
+    return `https://${process.env.CIRCLE_BRANCH}--${DOMAIN}`
+  }
+
+  return 'https://localhost:9000'
+}
+
 export default defineConfig({
   fixturesFolder: false,
   video: false,
-  e2e: {
-    baseUrl: process.env.CIRCLECI
-      ? process.env.CIRCLE_PR_NUMBER
-        ? `https://deploy-preview-${process.env.CIRCLE_PR_NUMBER}--destiny-clan-warfare.netlify.app`
-        : `https://${process.env.CIRCLE_BRANCH}--destiny-clan-warfare.netlify.app`
-      : 'https://localhost:9000'
-  }
+  e2e: { baseUrl: baseUrl() }
 })

c8b400270e5427c3c475b32f442e027801c46946
Ben Edwards <hello@benedfit.com>
2023-12-13 11:44:52 +0000

    chore: fix linting errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,7 @@ export const authOptions = {
           return await fetch(
             'https://www.bungie.net/platform/User/GetMembershipsForCurrentUser',
             {
+              // eslint-disable-next-line tsc/config
               headers: {
                 ...provider.httpOptions.headers,
                 authorization: `Bearer ${tokens.access_token}`
